98557c2bd81e32fdb24744d94a6e07e56857b77d
vshelke <vaibhavshelke017@gmail.com>
2020-04-30 09:53:41 +0530

    My changes

--- storeserv.py
+++ storeserv.py
@@ -6,7 +6,15 @@ bvtFile = open("ballots.list", 'r')
 Bid = bvtFile.read()
 bvtFile.close()
 
+def decrypt(m, e, n):
+    # print (m,e,n)
+    print (pow(int(m), int(e), int(n)))
+
 if bID == Bid:
-    votesFile = open("votes.data", 'w')
-    votesFile.write('%s, %s' % (bID, ev))
-    votesFile.close()
+    # voter_ballot = open("voterballots.list", "r")
+    # vid, ballot_id = voter_ballot.read().split(',')
+    vid, n, e = open('voters.pub', 'r').read().split(',')
+    decrypt(eb[3:-1], e, n)
+    # votesFile = open("votes.data", 'w')
+    # votesFile.write('%s, %s' % (ev, eb))
+    # votesFile.close()
